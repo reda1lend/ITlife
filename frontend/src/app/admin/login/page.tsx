cfa071826896397add31a2f2e565a233e0163444
reda1lend <pythonist19@gmail.com>
2026-02-19 14:52:04 +0300

    ebani rot cyka deploy fronta

--- frontend/src/app/admin/login/page.tsx
+++ frontend/src/app/admin/login/page.tsx
@@ -3,6 +3,9 @@
 import { useState } from "react";
 import { apiPost } from "../../../lib/api";
 import { setToken } from "../../../lib/auth";
+import ... from "../../../lib/auth";
+import ... from "../../../lib/api";
+
 
 export default function AdminLogin() {
   const [email, setEmail] = useState("admin@itlife.school");
